96b7a531897314ea9e71a9e4e327ae394b5b9dcb
Maa-mmy <dikecynthia22@gmail.com>
2023-01-03 03:30:03 +0100

    Copy - Cut - Paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = f"{Hol}"
-word_last_2 = f"{on}"
-middle_word = f"{olberto}"
+word_first_3 = 'Hol'
+word_last_2 = 'on'
+middle_word = 'olberto'
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
