3056f0a4a949cb259e18e0f09143965abc641a12
Maa-mmy <dikecynthia22@gmail.com>
2023-01-04 02:37:53 +0100

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for number in range(0, 100):
     if number == 99:
         print("{}".format(number))
     else:
-        print("{:14d}".format(number), end=", ")
+        print("{:4d}".format(number), end=", ")
