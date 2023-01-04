7f7f0f6896da45a0083a743bddc1fcffcb85ed68
Maa-mmy <dikecynthia22@gmail.com>
2023-01-04 03:42:37 +0100

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for number in range(0, 9):
     for number2 in range(1, 10):
         if number < number2:
-            if number == 8 and number 2 == 9:
+            if number == 8 and number2 == 9:
                 print("{}{}".format(number, number2))
             else:
                 print("{}{}".format(number, number2), end=", ")
