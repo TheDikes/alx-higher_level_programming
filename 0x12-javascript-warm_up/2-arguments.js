6b80062314a507b1cc30a146aa4869bcb7a8bf75
Maa-mmy <dikecynthia22@gmail.com>
2023-04-11 23:48:40 +0100

    Js Warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-
 const args = process.argv.slice(2);
 const numArgs = args.length;
-
 if (numArgs === 0) {
 	console.log('No argument');
 } else if (numArgs === 1) {
@@ -10,4 +8,3 @@ if (numArgs === 0) {
 } else {
 	console.log('Arguments found');
 }
-
