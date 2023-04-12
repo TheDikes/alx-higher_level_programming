4c47cee5ae65901f96943688bfc1ea59e71c8743
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-04-12 06:48:12 +0100

    Js Warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+const arg = +(process.argv[2]);
+
+if (!isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log('My number:', parseInt(process.argv[2]));
+  console.log(`My number: ${arg}`);
 }
