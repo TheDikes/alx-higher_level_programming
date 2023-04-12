48fd350cbba8b232a187646539d2bbd75ee425de
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-04-12 02:17:19 +0100

    Js Warm up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
 const x = +(process.argv[2]);
 
 if (isNaN(x)) {
-	consol.log('Missing number of occurrences');
+	console.log('Missing number of occurrences');
 } else {
 	for (let i = 0; i < x; i++) {
 		console.log('C is fun');
