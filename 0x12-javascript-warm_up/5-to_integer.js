ebe264cc6ed6978016462cdb2ab83d05943905de
Maa-mmy <dikecynthia22@gmail.com>
2023-04-12 09:39:35 +0100

    Js Warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,11 @@
-ii/usr/bin/node
+#!/usr/bin/node
 // code checks if argument passed is a number and prints it else, prints not a number.
 
 const arg = process.argv;
-const num = +(arg[2]);
+const number = parseInt(arg[2]);
 
-if (isNaN(num)) {
+if (isNaN(number)) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${num}`);
+  console.log('My number: ' + number);
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // This code prints lines using string array and loop.
 
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for(let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+for (let x = 0; x < 3; x++) {
+  console.log(str[x]);
 }
