578607189a0a1908c02529fd669ca9d38c4a4096
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-04-12 22:49:43 +0100

    Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (w <= 0 && h <= 0) {
       // If w or h is zero or negative, create an empty object
       return;
-    }
       this.width = w;
       this.height = h;
     }
   }
+}
 
 module.exports = Rectangle;
