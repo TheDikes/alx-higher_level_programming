bd09d1944529893e24897a52e2fd90b11d5bc926
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-04-12 07:21:01 +0100

    Js Warm up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorial(num) {
-  if (num === 0) {
-    return 1;
+  if (num === 0 || isNaN(num) {
+    return(1);
   }
-  return (num * factorial(num - 1));
+  return(num * factorial(num - 1));
 }
 
 console.log(factorial(+(process.argv[2])));
