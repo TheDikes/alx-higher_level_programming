3608d91bab87dfd34843c125da3d966ddd5bb997
Maa-mmy <dikecynthia22@gmail.com>
2023-04-12 05:59:01 +0100

    Js Warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < lines.length; i++) {
-	console.log(lines[i]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
