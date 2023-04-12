ee58b3d70521a917f2b863f0c75a6c3bef35319f
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-04-12 07:32:42 +0100

    Js Warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,4 +6,7 @@ const args = process.argv.slice(2).map(Number);
 if (args.length <= 1) {
   console.log(0);
 } else {
-  
+  const sortedArgs = args.sort((a, b) => b - a);
+  let secondBiggest = sortedArgs[1];
+  console.log(secondBiggest);
+}
