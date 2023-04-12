155779abf11677305fb14c90743b7d4343230048
Maa-mmy <dikecynthia22@gmail.com>
2023-04-12 01:07:37 +0100

    Js Warm up

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node 
-console.log(process.argv[2] + ' is ' + process.argv[3]);
+
+const [argv1, argv2] = process.argv.slice(2);
+
+console.log(`${argv1} is ${argv2}`);
---
+++ 0x12-javascript-warm_up/5-to_interger.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+const num = parseInt(arg);
+
+if (isNaN(num) || num === undefined) {
+	  console.log('Not a number');
+} else {
+	  console.log(`My number: ${num}`);
+}
