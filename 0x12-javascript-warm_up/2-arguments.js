b7f1385884610352a96f2df5d4c155f234749e28
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-04-12 01:16:57 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,11 +3,9 @@
 const args = process.argv.slice(2);
 const numArgs = args.length;
 
-if (numArgs === 0)
-{
+if (numArgs === 0) {
   console.log('No argument');
-} else if (numArgs === 1) 
-{
+} else if (numArgs === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
