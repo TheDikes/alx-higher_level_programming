89dd7c344f1db5d109aaaad628679317a7eb56c5
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-04-12 08:02:05 +0100

    Js Warm up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-const arg1 = process.arg[2];
-const arg2 = process.arg[3];
+//Prints the addition of two integers
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 
 function add(a, b) {
   sum = a + b;
-  console.log(sum);
+  return(sum);
 }
 
 add(Number(arg1), Number(arg2));
