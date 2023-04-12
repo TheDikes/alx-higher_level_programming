a4894892fbbd2121fd70051bb78d2a693d5805d2
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-04-12 08:17:45 +0100

    Js Warm up

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//This function is visible from outside and executes x times a function
+
+function callMeMoby(x, theFunction) {
+  for(let i = 0; i < x; i++) {
+    callMeMoby();
+  }
+   
+  exports = callMeMoby;
