db084ff3a644e04ec910f84c8c781362ed17853d
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-04-12 01:22:29 +0100

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
 const arg = process.argv[2];
-
 const num = parseInt(arg);
 
 if (isNaN(num) || num === undefined) {
-	  console.log('Not a number');
+	console.log('Not a number');
 } else {
-	  console.log(`My number: ${num}`);
+	console.log(`My number: ${num}`);
 }
