5288d8db61d2cb4a86a5c26a01c917bb07a7c6f5
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-04-12 02:23:08 +0100

    Js Warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const arg = process.argv[2];
 const num = +(arg);
 if (isNaN(num)) {
-	console.log('Not a number');
+console.log('Not a number');
 } else {
-	console.log(`My number: ${num}`);
+console.log(`My number: ${num}`);
 }
