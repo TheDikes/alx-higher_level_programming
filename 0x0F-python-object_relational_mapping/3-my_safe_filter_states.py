562b7c5d2da1ef369870726040fa78027eeab4f1
Maa-mmy <dikecynthia22@gmail.com>
2023-04-16 21:59:46 +0100

    Python - ORM

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,8 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    
-    cur.execute("SELECT * FROM states WHERE name =  %s ORDER BY states.id ASC",(sys.argv[4], ))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY states.id ASC",(sys.argv[4], ))
     states = cur.fetchall()
     for state in states:
         print(state)
