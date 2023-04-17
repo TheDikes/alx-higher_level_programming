28ea2cef89a7100a80dcb589eeb729caa4b9327f
Maa-mmy <dikecynthia22@gmail.com>
2023-04-17 08:13:34 +0100

    Python - ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,8 +13,9 @@ if __name__ == "__main__":
     city = (sys.argv[4], )
     query = '''SELECT cities.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name = %s ORDER BY cities.id ASC'''
+                WHERE states.name LIKE %s ORDER BY cities.id ASC'''
     cur.execute(query, city)
+
     rows = cur.fetchall()
     tmp = list(row[0] for row in rows)
     print(*tmp, sep=", ")
