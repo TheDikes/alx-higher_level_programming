8bd981be5e08c0da147c0901fb09bce00eb04721
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-04-19 10:37:31 +0100

    Python - ORM

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,7 +9,6 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """create an engine to connect to a database"""
-  
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
