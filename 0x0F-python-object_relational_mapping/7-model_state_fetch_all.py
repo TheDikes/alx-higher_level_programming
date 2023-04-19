04c958aedf92235e12b9c4602af5ab00a657a573
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-04-19 11:22:46 +0100

    Python - ORM

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-     """create a session factory"""
+    """create a session factory"""
     Session = sessionmaker(bind=engine)  
     session = Session()
     for instance in session.query(State).order_by(State.id):
