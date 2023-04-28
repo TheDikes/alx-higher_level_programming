867504e3b8620002cb356b4e7dd03309927f1a9b
Maa-mmy <dikecynthia22@gmail.com>
2023-04-28 01:51:51 +0100

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-# Find a peak in a list of unsorted integers
+"""Find a peak in a list of unsorted integers"""
 
 def find_peak(list_of_integers):
-    #Check if the list is empty
+    """Check if the list is empty"""
     if not list_of_integers:
         return None
 
-    #Check if the list has only one element
+    """Check if the list has only one element"""
     if len(list_of_integers) == 1:
         return list_of_integers[0]
 
-    #Check if the first or last element is a peak
+    """Check if the first or last element is a peak"""
     if list_of_integers[0] >= list_of_integers[1]:
         return list_of_integers[0]
     if list_of_integers[-1] >= list_of_integers[-2]:
         return list_of_integers[-1]
 
-    #Binary search for a peak in the middle
+    """Binary search for a peak in the middle"""
     left = 1
     right = len(list_of_integers) - 2
     while left <= right:
