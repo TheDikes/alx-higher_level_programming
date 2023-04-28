7f715c0f2c32086f1e576c133e0c10add32cdee1
Maa-mmy <dikecynthia22@gmail.com>
2023-04-28 03:02:47 +0100

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,8 +7,4 @@ if __name__ == "__main__"
 url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
-    x_request_id = response.getheader('X-Request-Id')
-    if x_request_id is not None:
-        print("X-Request-Id:", x_request_id)
-    else:
-        print("No X-Request-Id header found in the response.")
+    print(dict(response.headers).get("X-Request-Id"))
