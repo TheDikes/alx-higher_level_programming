56eabf5397c28099f094e9b24e30f521a531af47
Maa-mmy <dikecynthia22@gmail.com>
2023-04-28 13:59:32 +0100

    What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status."""
+import requests
+
+
+if __name__ == "__main__":
+    res = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
