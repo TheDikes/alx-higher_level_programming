a601f88dfc6f5baa2a8960bb47992403c633ec08
Maa-mmy <dikecynthia22@gmail.com>
2023-04-28 15:03:07 +0100

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,7 +9,10 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    email = sys.argv[2]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    payload = {'email': email}
+
+    res = requests.post(url, data=payload)
+
+    print(res.txt)
