3fdab80ed5696229324fc790b03140f5915ae54e
Maa-mmy <dikecynthia22@gmail.com>
2023-04-28 15:09:01 +0100

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,11 +8,12 @@ import sys
 import requests
 
 if __name__ == "__main__":
+
     url = sys.argv[1]
     email = sys.argv[2]
 
-    payload = {'email': email}
+    print(f"Your email is: {email}")
 
-    res = requests.post(url, data=payload)
+    response = requests.post(url, data={"email": email})
 
-    print(res.txt)
+    print(response.text)
