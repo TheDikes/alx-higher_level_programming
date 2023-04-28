7d28e13e0ae003a63f9c39dc6897e6c1cd18c651
Maa-mmy <dikecynthia22@gmail.com>
2023-04-28 02:49:56 +0100

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-import urllib.request
-
 """A script that
-  fetches https://alx-intranet.hbtn.io/status.
-  uses urlib package
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
 
-if __name__=='__main__':
 
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as res:
-        body = res.read()
+if __name__ == '__main__':
+    import urllib.request
 
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
