f072284e97cf7cf6c5900417ead29076cfbc9ab0
Maa-mmy <dikecynthia22@gmail.com>
2023-04-28 02:41:24 +0100

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import urllib.request
 
+if __name__=='__main__':
+
 url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-        body = response.read()
+with urllib.request.urlopen(url) as res:
+        body = res.read()
 
         print("Body response:")
         print("\t- type: {}".format(type(body)))
