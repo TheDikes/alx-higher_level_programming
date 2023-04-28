7f4f88de9dc8ceb86dbc02bfdfc6fe3078144e50
Maa-mmy <dikecynthia22@gmail.com>
2023-04-28 02:05:05 +0100

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-def find_peak(list_of_integers):
-        """Finds a peak in a list of unsorted integers."""
-            n = len(list_of_integers)
-                if n == 0:
-                            return None
-                            elif n == 1:
-                                        return list_of_integers[0]
-                                        elif n == 2:
-                                                    return max(list_of_integers)
-                                                    else:
-                                                                mid = n // 2
-                                                                        if list_of_integers[mid] < list_of_integers[mid-1]:
-                                                                                        return find_peak(list_of_integers[:mid])
-                                                                                            elif list_of_integers[mid] < list_of_integers[mid+1]:
-                                                                                                            return find_peak(list_of_integers[mid+1:])
-                                                                                                                else:
-                                                                                                                                return list_of_integers[mid]
-
+"""Finds a peak in a list of unsorted integers."""
+n = len(list_of_integers)
+if n == 0:
+    return None
+elif n == 1:
+    return list_of_integers[0]
+elif n == 2:
+    return max(list_of_integers)
+else:
+    mid = n // 2
+    if list_of_integers[mid] < list_of_integers[mid-1]:
+        return find_peak(list_of_integers[:mid])
+    elif list_of_integers[mid] < list_of_integers[mid+1]:
+        return find_peak(list_of_integers[mid+1:])
+    else:
+        return list_of_integers[mid]
