46edb986ee002866e164bc58de5440c77df669b2
Maa-mmy <dikecynthia22@gmail.com>
2023-04-28 03:16:17 +0100

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,10 +5,10 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {"email":sys.argv[2]}
 
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data=data, method='POST')
+    data = urllib.parse.urlencode(value).encode("ascii")
+    req = urllib.request.Request(url, data, method='POST')
 
     with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
