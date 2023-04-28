f756b84a55c88a2496fb5cbca220469e4c1ec90c
Maa-mmy <dikecynthia22@gmail.com>
2023-04-28 02:47:09 +0100

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import urllib.request
+
 """A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
+  fetches https://alx-intranet.hbtn.io/status.
+  uses urlib package
 """
 
 if __name__=='__main__':
 
 url = 'https://alx-intranet.hbtn.io/status'
-
 with urllib.request.urlopen(url) as res:
         body = res.read()
 
